3add8a77a33574a6f5c650f7f7da73aa86f59bed
«Valeria» <ldl1@list.ru>
2023-05-11 07:31:12 +0300

    add Ex.7

---
+++ C#Homework/Seminar1/Ex.7/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 7. Напишите программу, которая на входе принимает целое число любой разрядности, а на выходе выдает вторую слева цифру этого числа или говорит, что такой цифры нет.
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num < 0) Num = -Num;
+if (Num > 9) 
+{
+    while (Num > 99) 
+    {
+        Num = Num /10;
+    }
+    Console.WriteLine(Num % 10);
+}
+else Console.WriteLine("В этом числе нет второй цифры");
