b260b4c6e8bb08e1acab5a48693e1a57f44f9fa5
«Valeria» <ldl1@list.ru>
2023-05-11 07:24:25 +0300

    add Ex.6

---
+++ C#Homework/Seminar1/Ex.6/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 6. Напишите программу, которая принимает на вход число и выдает. является ли число четным.
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num == 0) Console.WriteLine("Вы ввели ноль");
+else if (Num % 2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
