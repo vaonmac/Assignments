098aa521220acc907c402c410fecc5848ceb6c2a
«Valeria» <ldl1@list.ru>
2023-06-01 12:55:52 +0300

    Seminar 4 exercises

---
+++ C#Homework/Seminar4/Ex.25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int Exponent (int Num1, int Num2)
+{
+    int result = Num1;
+    for (int i = 1; i < Num2; i++) result = result * Num1;
+    return result;
+}
+
+System.Console.WriteLine("Введите числа A и B");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Число {A} в натуральной степени {B}: {Exponent (A, B)}");
---
+++ C#Homework/Seminar4/Ex.29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void RandomArray8 ()
+{
+    int [] Array = new int[8];
+    int length = Array.Length;
+    Console.Write ("[");
+    for (int i = 0; i < length; i++) 
+    {
+        Array [i] = new Random().Next(0, 100);
+        Console.Write (Array [i]);
+        if (i < length-1) Console.Write (",");
+    }
+    Console.WriteLine ("]");
+}
+
+RandomArray8 ();
